e728410576f5aa6e2fc94d664fc3dfd0c73a7efe
Michael Anderson <Micmail566@gmail.com>
2017-01-10 13:58:44 -0500

    Nots from 1/10/2017

--- Program.cs
+++ Program.cs
@@ -10,6 +10,71 @@ namespace WeekOne
     {
         static void Main(string[] args)
         {
+            string[] board = {
+                "_", "_", "_", "_", "_", "_", "_", "_", "_"
+        };
+            int counter = 0;
+            foreach (string place in board)
+            {
+                Console.Write(" " + place);
+                if (counter == 2)
+                {
+                    Console.WriteLine("");
+                    counter = 0;
+                }
+                else
+                {
+                    counter++;
+                }
+
+            }
+            var MyString = "Mic";
+            var cleanString = MyString.Replace("i", "0");
+            int classSize = int.Parse(Console.ReadLine());
+            int[] ages = new int[classSize];
+
+            for (int i = 0; i < classSize; i++)
+            {
+                Console.WriteLine($"Age of {i} student?");
+                ages[i] = int.Parse(Console.ReadLine());
+
+                if (i == 2)
+                {
+                    Console.WriteLine("Money Maker");
+                }
+                else if (i == 3)
+                {
+                    Console.WriteLine("Money Taker");
+                }
+
+            }
+            foreach (var age in ages)
+            {
+                string readableDringAge;
+                bool isLegalToDrink = age >= 21;
+                if (isLegalToDrink)
+                {
+                    readableDringAge = "Yes";
+                }
+                else
+                {
+                    readableDringAge = "No";
+                }
+
+                readableDringAge = age >= 21 ? "Yes" : "No";
+                Console.Write($"{age} is legal to drink?{readableDringAge}");
+            }
+            Console.WriteLine("--------");
+            Console.WriteLine(ages.Max());
+            Console.WriteLine(ages.Min());
+            Console.WriteLine(ages.Average());
+
+
+
+            string[] myArray = new string[9];
+            myArray[0] = "X";
+
+            Console.WriteLine(myArray);
         }
     }
 }
